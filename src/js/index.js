99805572588f0891a5062cd3fa65942dbbc3eea8
alex-suschin <alex_suschin@mail.ru>
2020-07-22 10:54:12 +0300

    header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,45 @@
+import $ from "jquery";
+import Swiper from 'swiper';
 import "./import/modules";
+import "../../node_modules/bootstrap/js/dist/modal";
+import "./import/jquery.mask";
+
+
+
+$(function() {
+
+    $("input[type=tel]").mask("+7 (999) 999-9999");
+
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
+
+
+    $(window).on('load resize', function() {
+
+        var width = $(window).width();
+
+        if (width > '991') {
+
+        }
+    });
+
+});
