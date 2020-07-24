85e1670c475406b076729e8a66275cdf2912944d
alex-suschin <alex_suschin@mail.ru>
2020-07-24 21:10:13 +0300

    garanty

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,14 @@ Swiper.use([Navigation, Pagination]);
 import "./import/modules";
 import "../../node_modules/bootstrap/js/dist/modal";
 import "./import/jquery.mask";
+import "./import/jquery.nice-select.min";
+import "./import/datepicker.min";
 
 
 
 $(function() {
+
+    $('select').niceSelect();
 
     $("input[type=tel]").mask("+7 (999) 999-9999");
 
@@ -17,6 +21,18 @@ $(function() {
             clickable: true
         },
     });
+
+    $('.vars-item').click(function() {
+        $('.vars-items').find('.active').removeClass('active');
+        $(this).addClass('active');
+        $('.price-table').find('.price-table__elem').hide();
+        $('#' + $(this).data('switch')).show();
+    });
+
+    $('.date-input').datepicker({
+
+    });
+
 
 
     $(window).on('load resize', function() {
